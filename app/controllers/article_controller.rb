25ed313810a2a75b1eeb9dc50be85e3f6e9e9a8d
Pol-Erwan <polerwankite@gmail.com>
2020-03-21 17:01:16 +0100

    update article.show

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -5,6 +5,7 @@ class ArticleController < ApplicationController
     @events = Article.where(domain: "event")
     @portfolios = Article.where(domain: "portfolio")
     @tests = Article.find_by(domain: "test")
+    @article = Article.where(domain: "news").last(7)
     @news = @articles.last(4).reverse
     @products = Product.last(6)
 
@@ -13,6 +14,7 @@ class ArticleController < ApplicationController
   def show
     @article = Article.find(params[:id])
     @users = User.all
+    @news = Article.where(domain: "news").last(7).reverse
     @size = 1
     @name = ["","background","jumbotron","intro","intro 2","id card","competences","bonus"]
   end
